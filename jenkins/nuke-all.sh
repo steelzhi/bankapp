3137d74f5bacb02332727b55b906eb683f05f39e
Stanislav Zhiganov <zhi13@ya.ru>
2025-06-25 18:56:08 +0400

    Добавить заготовки в Jenkins для оставшихся 4 сервисов

--- jenkins/nuke-all.sh
+++ jenkins/nuke-all.sh
@@ -17,9 +17,13 @@ echo "Using DOCKER_REGISTRY: $DOCKER_REGISTRY"
 echo "Uninstalling Helm releases..."
 for ns in test prod; do
   helm uninstall accounts-service -n "$ns" || true
+  helm uninstall blocker-service -n "$ns" || true
+  helm uninstall cash-service -n "$ns" || true
+  helm uninstall exchange-service -n "$ns" || true
   helm uninstall exchange-generator-service -n "$ns" || true
   helm uninstall front-ui-service -n "$ns" || true
   helm uninstall notifications-service -n "$ns" || true
+  helm uninstall transfer-service -n "$ns" || true
   helm uninstall postgres -n "$ns" || true
 done
 
@@ -39,9 +43,14 @@ docker volume rm jenkins_home || true
 
 echo "Removing images..."
 docker image rm ${DOCKER_REGISTRY}/accounts-service:1 || true
+docker image rm ${DOCKER_REGISTRY}/blocker-service:1 || true
+docker image rm ${DOCKER_REGISTRY}/cash-service:1 || true
+docker image rm ${DOCKER_REGISTRY}/exchange-service:1 || true
 docker image rm ${DOCKER_REGISTRY}/exchange-generator-service:1 || true
 docker image rm ${DOCKER_REGISTRY}/front-ui-service:1 || true
 docker image rm ${DOCKER_REGISTRY}/notifications-service:1 || true
+docker image rm ${DOCKER_REGISTRY}/transfer-service:1 || true
+
 docker image rm jenkins/jenkins:latest || true
 
 echo "Pruning system..."
